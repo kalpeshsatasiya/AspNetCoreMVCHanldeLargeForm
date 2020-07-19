d7cf884dae71f355cd943df80ca8ed6d30a6d311
Kalpesh Satasiya <satasiya.kalpesh2006@gmail.com>
2020-07-20 00:35:16 +0530

    intial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HandleLargeForm.Models;
+
+namespace HandleLargeForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new List<int>();
+            for (int i = 0; i < 1100; i++)
+            {
+                model.Add(i);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(List<int> model)
+        {
+
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
